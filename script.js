ee823f50b8313300b5d7fded4bda4e5ee778d067
Ashik-Himel <ashikujjamanhimel@gmail.com>
2022-01-19 21:56:17 +0600

    Contact Info

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function navClose() {
   navbarCollapse.classList.remove('show');
 }
 navlink.forEach(n => n.addEventListener('click', navClose));
+
+// Isotope JS
+$('.tab').isotope({
+  itemSelector: '.tab-item',
+  masonry: {}
+});
+$('.tab-titles span').click(function(){
+  $('.tab-titles span').removeClass('active');
+  $(this).addClass('active');
+
+  var selector = $(this).attr('data-filter');
+  $('.tab').isotope({
+      filter:selector
+  });
+  return false;
+});

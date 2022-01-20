bbb919ca751778481ed038ece7ed5cf5b15fc2a5
Ashik-Himel <ashikujjamanhimel@gmail.com>
2022-01-20 21:30:43 +0600

    chat plugin updated

--- script.js
+++ script.js
@@ -27,23 +27,3 @@ $('.tab-titles span').click(function(){
 $('.counter').counterUp({
   time: 1000
 });
-
-//Messenger Chat Plugin
-var chatbox = document.getElementById('fb-customer-chat');
-chatbox.setAttribute("page_id", "110159741559451");
-chatbox.setAttribute("attribution", "biz_inbox");
-
-window.fbAsyncInit = function() {
-  FB.init({
-    xfbml            : true,
-    version          : 'v12.0'
-  });
-};
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
